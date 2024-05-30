644cefd6c1ac5df9325e3c8803e86683b1283618
DandiRizkyy <dandirizkyy94@gmail.com>
2024-05-30 13:04:57 +0700

    update nextjs config add image domain

--- next.config.js
+++ next.config.js
@@ -10,6 +10,11 @@ const nextConfig = {
       'www.exscribe.com',
       's3-ap-southeast-1.amazonaws.com',
       's-light.tiket.photos',
+      'encrypted-tbn2.gstatic.com',
+      'awsimages.detik.net.id',
+      'encrypted-tbn3.gstatic.com',
+      'encrypted-tbn0.gstatic.com',
+      'soundcorners.com',
     ],
   },
 };
